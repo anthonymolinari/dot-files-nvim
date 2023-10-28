30c3f19fe8030a9c14abe9cde5baa0e30cfe073a
anthonymolinari <molinaria@sonoma.edu>
2023-10-28 09:19:28 -0700

    lazygit macro
    added lazygit macro, hide/show floating terminal running lazygit

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -8,3 +8,11 @@ tg.setup{
     direction = 'horizontal',
 }
 
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
+
+function _lazygit_toggle()
+    lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
