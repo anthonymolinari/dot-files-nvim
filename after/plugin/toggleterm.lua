269c92188356832fa62ea072325abfc27f937715
anthonymolinari <molinaria@sonoma.edu>
2023-10-29 21:56:14 -0700

    tab terminal

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -11,11 +11,12 @@ tg.setup{
 local opts = { noremap = true, silent = true }
 local Terminal = require('toggleterm.terminal').Terminal
 
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float'})
 local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, direction = 'float'})
 local ranger = Terminal:new({ cmd = "ranger", hidden = true, direction = 'float' })
 local htop = Terminal:new({ cmd = "htop", hidden = true, direction = 'float' })
 local basic = Terminal:new({ hidden = true, direction = 'float' })
+local tab = Terminal:new({ hidden = true, direction = 'tab'})
 
 function _lazygit_toggle()
     lazygit:toggle()
@@ -32,8 +33,12 @@ end
 function _basic_toggle()
     basic:toggle()
 end
+function _tab_toggle()
+    tab:toggle()
+end
 
 vim.api.nvim_set_keymap("n", "<C-A-t>", "<cmd>lua _basic_toggle()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>lua _tab_toggle()<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>lua _ranger_toggle()<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua _lazydocker_toggle()<CR>", opts)
