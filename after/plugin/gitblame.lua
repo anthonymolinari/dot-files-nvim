7e73a010f4714c5e01bf8356e95a4198b7bd5c7c
anthonymolinari <molinaria@sonoma.edu>
2023-11-16 23:00:09 -0800

    git blame configuration & updated colorscheme

--- after/plugin/gitblame.lua
+++ after/plugin/gitblame.lua
@@ -1,4 +1,11 @@
-require('gitblame').setup{
+local blame = require('gitblame')
+
+blame.setup{
     enabled = true,
-    message_template = '<author>: <summary> | <date>'
+    message_template = ' <author>: <summary> | <date>',
+    message_when_not_committed = ''
 }
+
+local opts = {}
+vim.keymap.set('n', '<leader>gb', vim.cmd.GitBlameToggle, opts)
+vim.keymap.set('n', '<leader>gtb', vim.cmd.GitBlameOpenFileURL, opts)
