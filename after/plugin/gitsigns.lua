b7dd9c9d610fe83f428b3a994ec11110c058e29d
anthonymolinari <molinaria@sonoma.edu>
2023-11-19 15:59:18 -0800

    gitsigns config

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,1 +1,67 @@
---require('gitsigns').setup()
+---@diagnostic disable: undefined-global
+require('gitsigns').setup({
+    signcolumn = true,
+    numhl = false,
+    linehl = false,
+    word_diff = true,
+    watch_gitdir = {
+        follow_files = true,
+    },
+    attach_to_untracked = true,
+    current_line_blame = true,
+    current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'right_align',
+        delay = 200,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+    },
+    sign_priority = 6,
+    update_debounce = 100,
+    status_formatter = nil,
+    max_file_length = 400000,
+    preview_config = {
+        border = 'single',
+        style = 'minimal',
+        relative = 'cursor',
+        row = 0,
+        col = 1,
+    },
+    yadm = {
+        enable = false,
+    },
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- navigation
+        map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+        end, {expr=true})
+
+        map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+        end, {expr=true})
+
+        -- actions
+        map('n', '<leader>hs', gs.stage_hunk)
+        map('n', '<leader>hr', gs.reset_hunk)
+        map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('n', '<leader>hS', gs.stage_buffer)
+        map('n', '<leader>hu', gs.undo_stage_hunk)
+        map('n', '<leader>hR', gs.reset_buffer)
+        map('n', '<leader>hp', gs.preview_hunk)
+        map('n', '<leader>dt', gs.diffthis)
+        map('n', '<leader>dt', gs.diffthis)
+        map('n', '<leader>td', gs.toggle_deleted)
+    end,
+})
