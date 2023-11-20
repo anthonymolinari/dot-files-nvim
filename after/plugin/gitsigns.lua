4dd76c224b5b59f09b771a70f7e4858ae71f1793
anthonymolinari <molinaria@sonoma.edu>
2023-11-20 10:47:57 -0800

    re-enable gitsigns

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,6 +1,4 @@
 ---@diagnostic disable: undefined-global
-require('gitsigns').setup()
---[[
 require('gitsigns').setup({
     signcolumn = true,
     numhl = false,
@@ -66,5 +64,4 @@ require('gitsigns').setup({
         map('n', '<leader>td', gs.toggle_deleted)
     end,
 })
-]]--
 
