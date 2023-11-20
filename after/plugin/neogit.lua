18985c3d43c76dcdb0ed169b3249a9f2e86b8904
anthonymolinari <molinaria@sonoma.edu>
2023-11-20 15:10:07 -0800

    updates again

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -1,7 +1,11 @@
 ---@diagnostic disable: undefined-global
 local neogit = require('neogit')
 
-neogit.setup()
+neogit.setup({
+    mappings = {
+        ["?"] = "HelpPopup",
+    }
+})
 
 vim.keymap.set('n', '<leader>ng', neogit.open, {});
 vim.keymap.set('n', '<leader>nc', function() neogit.open({ 'commit' }) end, {});
